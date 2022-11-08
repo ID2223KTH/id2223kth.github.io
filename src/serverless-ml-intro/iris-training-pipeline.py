4844c1d1fee4295e64d12ce23a6c3d2758c75443
Fabian Schmidt <fschm@kth.se>
2022-11-08 10:32:30 +0100

    Update API key to be consistent with slides

--- src/serverless-ml-intro/iris-training-pipeline.py
+++ src/serverless-ml-intro/iris-training-pipeline.py
@@ -7,7 +7,7 @@ if LOCAL == False:
    stub = modal.Stub()
    image = modal.Image.debian_slim().apt_install(["libgomp1"]).pip_install(["hopsworks", "seaborn", "joblib", "scikit-learn"])
 
-   @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("jim-hopsworks-ai"))
+   @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
    def f():
        g()
 
